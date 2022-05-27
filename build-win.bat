fb4ea21973d2ae26e49dd635f18382c256ec2903
aaron <qkrsogusl3@gmail.com>
2022-05-27 18:28:31 +0900

    add env params

--- build-win.bat
+++ build-win.bat
@@ -1,3 +1,3 @@
 if exist codecommit-buildkite.zip del /f codecommit-buildkite.zip
 
-zip.exe -r ./codecommit-buildkite.zip ./index.js ./node_modules
+zip.exe -r ./codecommit-buildkite.zip ./index.js ./node_modules/axios ./node_modules/debug ./node_modules/follow-redirects ./node_modules/is-buffer ./node_modules/ms
--- index.js
+++ index.js
@@ -5,31 +5,36 @@ const codecommit = new AWS.CodeCommit()
 
 
 const handleReference = async (repositoryName, reference) => {
-    
+
     const buildkiteURL = `https://api.buildkite.com/v2/organizations/${process.env.BUILDKITE_ORG}/pipelines/aws-${repositoryName.toLowerCase()}/builds`
-    
+
     const { commit, ref } = reference
-    
+
     const match = ref.match(/^refs\/heads\/(.*)/)
     if (!match) {
         return false
     }
     const branch = match[1]
     const { commit: { message, author } } = await codecommit.getCommit({ repositoryName, commitId: commit }).promise()
-    
+
+    const env = {
+        "BUILD_TARGET": "Android"
+    };
+
     const params = {
         commit,
         branch,
         message,
-        author
+        author,
+        env
     }
-    
+
     const response = await axios.post(buildkiteURL, params, {
         headers: {
             Authorization: `Bearer ${process.env.BUILDKITE_TOKEN}`
         }
     })
-    
+
     return {
         params,
         response: {
